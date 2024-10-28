8c567e9f03a5b4b5eaf0e0e8f7fc37097034c44f
Farman Pirzada <farman@fueled.io>
2024-10-28 19:19:05 +0000

    Ready for second review

--- models/gold/gold_meta_ads_campaign_reporting.sql
+++ models/gold/gold_meta_ads_campaign_reporting.sql
@@ -1,32 +1,11 @@
 select
-    ad_id,
-    ad_name,
+    date(date_start) as campaign_date,
     campaign_id,
     campaign_name,
-    objective,
-    date(date_start) as spend_date,
-    sum(spend) as daily_spend,
-    sum(clicks) as daily_clicks,
-    sum(impressions) as daily_impressions,
-    avg(costs_per_link_click) as avg_costs_per_link_click,
-    avg(costs_per_thousand_impressions) as avg_costs_per_thousand_impressions,
-    avg(click_through_rate) as avg_click_through_rate,
-    spend_cap,
-    daily_budget,
-    lifetime_budget,
-    effective_status,
-    buying_type
+    sum(spend) as daily_ad_spend,
+    sum(clicks) as total_daily_clicks,
+    sum(impressions) as total_daily_impressions,
+-- TODO: Need conversions and conversions_value
 from {{ ref("canonical_meta_ads_campaigns") }}
-group by
-    ad_id,
-    ad_name,
-    campaign_id,
-    campaign_name,
-    objective,
-    spend_date,
-    spend_cap,
-    daily_budget,
-    lifetime_budget,
-    effective_status,
-    buying_type
-ORDER BY spend_date DESC, campaign_name DESC
+group by campaign_id, campaign_name, campaign_date
+order by campaign_date desc, campaign_name desc
